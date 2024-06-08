77d67de9dbb3308c7d39d36a9ccc3e02feb0750e
Dina EL-naggar <420200118@sha.edu.eg>
2024-06-08 15:48:42 +0300

    Initial commit3

--- DB/connection.js
+++ DB/connection.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 
-export const sequelize = new Sequelize('assignment6', 'root', '',{
-    host: 'localhost',
+export const sequelize = new Sequelize('bonrxrjqoytnpvd2kxzx', 'ue4zublz5zjglryf', 'bp37jTthMCeLaFGEZBn3',{
+    host: 'bonrxrjqoytnpvd2kxzx-mysql.services.clever-cloud.com',
     dialect: 'mysql'
 });
 
